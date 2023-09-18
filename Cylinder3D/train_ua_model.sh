be04f3aa789d4de8e0ab809acab57eda3acabb66
Mason McVicker <mmcvicker@crimson.ua.edu>
2023-09-18 15:34:54 -0500

    Remove train script and update SK config

--- Cylinder3D/train_ua_model.sh
+++
@@ -1,5 +0,0 @@
-name=cylinder_asym_networks_rellis
-gpuid=0
-
-CUDA_VISIBLE_DEVICES=${gpuid}  python -u train_cylinder_asym.py --config_path config/rellis.yaml \
-2>&1 | tee logs_dir/${name}_logs_tee.txt
--- SphereFormer/train_ua_model.sh
+++
@@ -1,1 +0,0 @@
-python train.py --config config/semantic_kitti/rellis_unet32_spherical_transformer.yaml
